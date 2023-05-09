18095cf26955d55ae34bf83023f5cfa89fe8ab26
Lidushh <fedosova00@gmail.com>
2023-05-09 08:12:06 +0300

    in com

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello World")
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+money = int(input('Введите сумму вклада: '))
+
+for key in per_cent:
+    deposit.append(per_cent[key] * money / 100)
+    print(deposit)
+max_deposit = max(deposit)
+print('Максимальная сумма, которую вы можете заработать — ' + str(max_deposit))
